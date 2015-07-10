b79c30213828c6c2d48096b9fbb5fd0b04837310
Christopher Hofmann <christopherushofmann@googlemail.com>
2015-07-10 22:49:49 +0200

    measured and added all useful keys

--- main.c
+++ main.c
@@ -179,6 +179,10 @@ usbMsgLen_t usbFunctionWrite(uint8_t * data, uchar len) {
 #define STATE_SEND_KEY 1
 #define STATE_RELEASE_KEY 2
 
+void sendToKeyboard() {
+    
+}
+
 void key_down(uint8_t down_key) {
     /* we only add a key to our pressed list, when we have less than 6 already pressed.
     ** additional keys will be ignored  */
@@ -208,12 +212,120 @@ void key_up(uint8_t up_key) {
     keysHaveChanged = 1; 
 }
 
+void toggleModifier(uchar key) {
+    keyboard_report.modifier ^= key;
+}
+
 
 uint8_t map(uint8_t keyCodeIn) {
     switch ((keyCodeIn & ~(0x01))) {
         case 0x4c : return 0x04; // a
         case 0x8c : return 0x16; // s
         case 0x0c : return 0x07; // d
+        case 0x92 : return 0x14; // q
+        case 0x12 : return 0x1a; // w
+        case 0xE2 : return 0x08; // e
+        case 0x62 : return 0x15; // r
+        case 0xA2 : return 0x17; // t
+        case 0x22 : return 0x1C; // y (z-key)
+        case 0xC2 : return 0x18; // u
+        case 0x42 : return 0x0c; // i
+        case 0x82 : return 0x12; // o
+        case 0x02 : return 0x13; // p
+        case 0xFC : return 0x2f; // [ (ü-key)
+        case 0x7C : return 0x30; // ] (pound key)
+        case 0x2a : return 0x2a; // backspace
+        case 0x64 : return 0x28; // enter
+        case 0x88 : toggleModifier((1 << 5)); return 0xFF; // right shift
+        case 0x60 : return 0x2c; // spacebar
+        case 0xe6 : return 0x50; // leftArrow
+        case 0x26 : return 0x51; // downarrow
+        case 0xc6 : return 0x4f; // rightArrow
+        case 0xd6 : return 0x52; // uparrow
+        case 0x84 : return 0x62; // keypad 0
+        case 0xb2 : return 0x63; // keypad ,
+        case 0xa4 : return 0x58; // keypad enter
+        case 0xe0 : toggleModifier((1 << 3)); return 0xFF;// meta left
+        case 0x36 : toggleModifier((1 << 2)); return 0xFF; // alt left
+        case 0xcc : toggleModifier((1 << 0)); return 0xFF; // ctrl left
+        case 0x38 : toggleModifier((1 << 1)); return 0xFF; // shift left
+        case 0x10 : return 0x39; // capslock
+        case 0x52 : return 0x2b; // tabulator
+        case 0xb8 : return 0x53; // num lock
+        case 0xf4 : return 0x09; // f
+        case 0x74 : return 0x0A; // g
+        case 0xb4 : return 0x0b; // h
+        case 0x34 : return 0x0D; // j
+        case 0xd4 : return 0x0e; // k
+        case 0x54 : return 0x0f; // l
+        case 0x94 : return 0x33; // ; ö
+        case 0x14 : return 0x34; // ' ä
+        case 0xd8 : return 0x1d; // z
+        case 0x58 : return 0x1b; // x
+        case 0x98 : return 0x06; // c
+        case 0x18 : return 0x19; // v
+        case 0xe8 : return 0x05; // b
+        case 0x68 : return 0x11; // n
+        case 0xa8 : return 0x10; // m
+        case 0x28 : return 0x36; // ,
+        case 0xc8 : return 0x37; // .
+        case 0x48 : return 0x38; // /
+        case 0x5e : return 0x3a; // F1
+        case 0x9e : return 0x3b; // 
+        case 0xee : return 0x3c; //
+        case 0xae : return 0x3d; //
+        case 0xce : return 0x3e; //
+        case 0x8e : return 0x3f; //
+        case 0xf5 : return 0x40; //
+        case 0xe5 : return 0x41; //
+        case 0xb5 : return 0x42; //
+        case 0x1e : return 0x43; //
+        case 0x6e : return 0x44; //
+        case 0x2e : return 0x45; // F12
+        case 0xaa : return 0x35; // `
+        case 0x86 : return 0x1e; // 1
+        case 0x06 : return 0x1f; // 
+        case 0xfa : return 0x20; //
+        case 0x7a : return 0x21; //
+        case 0xba : return 0x22; //
+        case 0x3a : return 0x23; //
+        case 0xda : return 0x24; //
+        case 0x5a : return 0x25; //
+        case 0x9a : return 0x26; //
+        case 0x1a : return 0x27; // 0
+        case 0xea : return 0x2D; // -
+        case 0x6a : return 0x2E; // =
+        case 0x46 : return 0x29; // esc
+        case 0x96 : return 0x46; // printscrn
+        case 0x16 : return 0x47; // scroll lock
+        case 0x56 : return 0x48; // pause
+        case 0x4a : return 0x7f; // mute
+        case 0xb7 : return 0x81; // vol down
+        case 0xd7 : return 0x80; // vol up 
+        case 0xf2 : return 0x66; // power
+        case 0x8a : return 0x54; // keypad /
+        case 0x0a : return 0x55; // keypad *
+        case 0x1c : return 0x56; // keypad -
+        case 0x4d : return 0x57; // keypad +
+        case 0xF0 : return 0x59; // keypad 1
+        case 0x70 : return 0x5a; //
+        case 0xb0 : return 0x5b; //
+        case 0x24 : return 0x5c; //
+        case 0xc4 : return 0x5d; //
+        case 0x44 : return 0x5e; //
+        case 0xdc : return 0x5f; //
+        case 0x5c : return 0x60; //
+        case 0x9c : return 0x61; // keypad 9
+        case 0xc9 : return 0x49; // insert
+        case 0xd2 : return 0x4a; // home
+        case 0xF8 : return 0x4b; // PgUp
+        case 0xF8 : return 0x4b; // delete
+        case 0xF8 : return 0x4b; // end
+        case 0xbc : return 0x4c; // PgDn
+        case 0xac : return 0x4d;
+        case 0x20 : return 0x4e;
+
+
         default : return 0xFF; // return error code
     }
 }
@@ -250,6 +362,9 @@ void emergencyParse() {
     // for now lets just assume, that this only happens when multiple keys are pressed and one of them comes back up
     // for an unknown reason there is no break command for that key, only for the last one of that sequence that comes back up
     key_up(map((uint8_t)readFromKeyboard));
+    readFromKeyboard = 0;
+    wiggle(4);
+    displayValue(readFromKeyboard);
 }
 
 
